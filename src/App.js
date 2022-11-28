f3b3ee70f3ed918a0b54bff503d5b80fdcd8e138
KelvinYang <kelvin.yang@bybit.com>
2022-11-28 22:45:49 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box, Card, createTheme, CssBaseline, Grid, ThemeProvider} from "@mui/material";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+            <Grid container spacing={2} padding={5} columns={{xs: 4, sm: 8, md: 12}}>
+                <Grid item xs={12} display="flex" justifyContent="center" alignItems="center">
+                    <Box
+                        component="img"
+                        sx={{
+                            borderRadius: "50%",
+                            height: 200,
+                            // width: 350,
+                            // maxHeight: { xs: 233, md: 167 },
+                            // maxWidth: { xs: 350, md: 250 },
+                        }}
+                        alt="The house from the offer."
+                        src="photo.jpeg"
+                    />
+                </Grid>
+                <Grid container xs={12} display="flex" justifyContent="center" alignItems="center">
+                    <Grid item lg={12} md={12} xs={12}>
+                        <h1 align={"center"}>Hello, I am Kelvin</h1>
+                    </Grid>
+                    <Grid item lg={12} md={12} xs={12}>
+                        <h3 align={"center"}>I am currently working as a Backend Development Engineer in Bybit</h3>
+                    </Grid>
+                    <Grid item>
+                        <a href={"https://www.bybit.com/"} target={"_blank"}>
+                            <Box
+                                component="img"
+                                sx={{height: 70}}
+                                alt="Bybit Logo"
+                                src="bybit-official-image.png"/>
+                        </a>
+                    </Grid>
+                </Grid>
+                <Grid container display="flex" justifyContent="center" alignItems="center">
+                    <Grid item padding={5} xs={12} md={4} lg={3}>
+                        <Card variant="outlined" sx={{padding:5, height:400}}>
+                            <h2>Frontend:</h2>
+                            <h5>React.js</h5>
+                            <h5>HTML</h5>
+                            <h5>CSS</h5>
+                            <h5>JavaScript</h5>
+                        </Card>
+                    </Grid>
+                    <Grid item padding={5} xs={12} md={4} lg={3}>
+                        <Card variant="outlined" sx={{padding:5, height:400}}>
+                            <h2>Backend:</h2>
+                            <h5>Java</h5>
+                            <h5>SpringBoot</h5>
+                            <h5>NodeJS</h5>
+                            <h5>ExpressJs</h5>
+                            <h5>MySQL</h5>
+                            <h5>Redis</h5>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+        </ThemeProvider>
+    );
 }
 
 export default App;
