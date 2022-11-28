abac5c7ac58d89a682d14673b5e722fa26396b58
KelvinYang <kelvin.yang@bybit.com>
2022-11-29 00:44:11 +0800

    feat: dark mode

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import './App.css';
-import {Box, Card, createTheme, CssBaseline, Grid, ThemeProvider} from "@mui/material";
+import {Box, Card, createTheme, CssBaseline, Grid, Switch, ThemeProvider} from "@mui/material";
 import ImageBox from "./component/ImageBox";
 import ContactCard from "./component/ContactCard";
 import SkillsCard from "./component/SkillsCard";
-
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
-});
-
+import {useCallback, useMemo, useState} from "react";
 
 function App() {
 
     const frontendSkills = ["React.js", "HTML", "CSS", "JavaScript"];
     const backendSkills = ["Java", "SpringBoot", "NodeJS", "ExpressJS", "MySQL", "Redis"];
+    const [darkMode, setDarkMode] = useState(true);
+    const toggleSwitch = useCallback(
+        () => {
+                setDarkMode(!darkMode)
+        },
+        [darkMode],
+    );
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: darkMode ? 'dark' : 'light',
+                },
+            }),
+        [darkMode],
+    );
+
 
     return (
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme={theme}>
             <CssBaseline/>
             <Grid container spacing={2} padding={5} columns={{xs: 4, sm: 8, md: 12}}>
                 <Grid item xs={12} display="flex" justifyContent="center" alignItems="center">
                     <ImageBox
                         height={200}
                         borderRadius={50}
-                        altText={"The house from the offer."}
+                        altText={"My Profile Picture"}
                         src={"photo.jpeg"}
                     />
                 </Grid>
-                <Grid container xs={12} display="flex" justifyContent="center" alignItems="center">
+                <Grid container display="flex" justifyContent="center" alignItems="center">
                     <Grid item lg={12} md={12} xs={12}>
-                        <h1 align={"center"}>Hello, I am Kelvin</h1>
+                        <h1 align={"center"}>Hello, I am YANG Kwan Ki, Kelvin</h1>
                     </Grid>
                     <Grid item lg={12} md={12} xs={12}>
                         <h3 align={"center"}>I am currently working as a Backend Development Engineer in Bybit</h3>
@@ -68,7 +79,7 @@ function App() {
                     <ContactCard
                         url={"https://github.com/kelvinykk511/"}
                         altText={"GitHub Logo"}
-                        src={"GitHub-Mark-Light-120px-plus.png"}
+                        src={darkMode ? "GitHub-Mark-Light-120px-plus.png" : "GitHub-Mark-120px-plus.png"}
                         title={"GitHub"}
                     />
                     <ContactCard
@@ -76,6 +87,14 @@ function App() {
                         altText={"Gmail Logo"}
                         src={"gmail.png"}
                         title={"GMail"}
+                    />
+                </Grid>
+                <Grid container display="flex" justifyContent="center" alignItems="center">
+                    <h3>Dark Mode</h3>
+                    <Switch
+                        onChange={toggleSwitch}
+                        sx={{marginTop: 0}}
+                        defaultChecked
                     />
                 </Grid>
             </Grid>
--- src/component/SkillsCard.js
+++ src/component/SkillsCard.js
@@ -5,7 +5,7 @@ const SkillsCard = ({title, skills})=>{
         <Grid item padding={5} xs={12} md={4} lg={3}>
             <Card variant="outlined" sx={{padding: 5, height: 400}}>
                 <h2>{title}:</h2>
-                {skills.map((skill, index) => (<h5>{skill}</h5>))}
+                {skills.map((skill, index) => (<h5 key={skill}>{skill}</h5>))}
             </Card>
         </Grid>
     )
